df967909983a2021e5acfcaf9d06f3a50255a235
servosdesign <servos.design0@gmail.com>
2022-06-10 19:38:19 -0500

    moved meal list data to backend

--- src/components/meals-available/meals-available.component.jsx
+++ src/components/meals-available/meals-available.component.jsx
@@ -3,34 +3,9 @@ import MealItem from '../meals/meal-item/meal-item.component';
 
 import { StyledDiv } from './meals-available.styles';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const MealsAvailable = () => {
+
+
   const mealsList = DUMMY_MEALS.map(meal => ( 
     <MealItem 
       id={meal.id}
