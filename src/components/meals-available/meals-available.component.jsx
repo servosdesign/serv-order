8ce3dc6602d538f5942fc20ea06fa261a5f598ce
servosdesign <servos.design0@gmail.com>
2022-06-12 18:44:54 -0500

    using backend data with useEffect hook

--- src/components/meals-available/meals-available.component.jsx
+++ src/components/meals-available/meals-available.component.jsx
@@ -1,19 +1,41 @@
+import { useEffect, useState } from 'react';
+
 import Card from '../card/card.component';
 import MealItem from '../meals/meal-item/meal-item.component';
 
 import { StyledDiv } from './meals-available.styles';
 
 const MealsAvailable = () => {
+  const [meals, setMeals] = useState([]);
 
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://servor-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
 
-  const mealsList = DUMMY_MEALS.map(meal => ( 
-    <MealItem 
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map(meal => (
+    <MealItem
       id={meal.id}
-      key={meal.id} 
-      name={meal.name} 
-      description={meal.description} 
-      price={meal.price} 
-   />
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
